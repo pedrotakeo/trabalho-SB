b56b32be56dff34fd200e7ae0a4ef2f758d694c2
Camila <camiyshibata@gmail.com>
2025-10-22 11:36:41 -0300

    procedimento principal

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+
+extern void setup_brk();
+extern void dismiss_brk();
+extern long get_brk();
+extern unsigned long int initial_brk;
+
+int main() {
+
+    printf("setup_brk()\n");
+    setup_brk();
+
+    printf("valor de initial_brk: %p\n", (void*)initial_brk);
+    printf("%p \n", (void*)get_brk());
+
+    sbrk(4096);
+    printf("brk modificado: %p \n", (void*)get_brk());
+
+    printf("dismiss_brk()\n");
+    dismiss_brk();
+    printf("valor de initial_brk: %p\n", (void*)initial_brk);
+
+    printf("%p \n", (void*)get_brk());
+
+
+    return 0;
+}
---
+++ makefile
@@ -0,0 +1,23 @@
+TARGET = main
+
+CC = gcc
+CFLAGS = -Wall -Wextra
+
+ASM = nasm
+ASMFLAGS = -f elf64 
+
+OBJS = main.o heap.o
+
+all: $(TARGET)
+
+$(TARGET): $(OBJS)
+	$(CC) -o $(TARGET) $(OBJS)
+
+main.o: main.c
+	$(CC) $(CFLAGS) -c main.c -o main.o
+
+heap.o: heap.s
+	$(ASM) $(ASMFLAGS) heap.s -o heap.o
+
+clean:
+	rm -f $(TARGET) $(OBJS)
