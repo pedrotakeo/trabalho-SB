d09aec3ea9a666b06a31d195c729f4596f7073e2
pedrotakeo <pts24@inf.ufpr.br>
2025-11-02 12:01:54 -0300

    finished alloc and free with incorrect return logic

--- heap.s
+++ heap.s
@@ -1,12 +1,14 @@
 section .bss
-    global initial_brk
-    initial_brk: resq 1
-    current_brk: resq 1
+    global ini_brk
+    ini_brk: resq 1
+    cur_brk: resq 1
 
 section .text
     global setup_brk
     global dismiss_brk
     global get_brk
+    global memory_alloc
+    global memory_free
 
 setup_brk:
     push rbp
@@ -16,8 +18,8 @@ setup_brk:
     xor rdi, rdi
     syscall
 
-    mov QWORD [initial_brk], rax
-    mov QWORD [current_brk], rax
+    mov QWORD [ini_brk], rax
+    mov QWORD [cur_brk], rax
 
     pop rbp
     ret
@@ -26,7 +28,7 @@ dismiss_brk:
     push rbp
     mov rbp, rsp
 
-    mov rdi, QWORD [initial_brk]
+    mov rdi, QWORD [ini_brk]
 
     mov rax, 12
     syscall
@@ -52,9 +54,9 @@ worst_fit_address:
     push rbp
     mov rbp, rsp
 
-    mov rax, QWORD [initial_brk]    ; rax = largest available space in heap (if any)
-    mov rbx, QWORD [current_brk]    ; current brk position (end of heap)
-    mov r10, QWORD [initial_brk]    ; address iterator
+    mov rax, QWORD [ini_brk]    ; rax = largest available space in heap (if any)
+    mov rbx, QWORD [cur_brk]    ; current brk position (end of heap)
+    mov r10, QWORD [ini_brk]    ; address iterator
 
     xor rcx, rcx                  ; size of max block found (initialized as 0)
 
@@ -63,13 +65,13 @@ wfa_loop:
     jge wfa_endloop  ; while (addressR10 < currentBRK)
 
     cmp BYTE [r10], 1
-    je wfa_fini_loop  ;if (use = 1) go to "i++"
+    je wfa_fini_loop  ;if (use = 1)
 
     cmp QWORD [r10 + 1], rdi
-    jl wfa_fini_loop
+    jl wfa_fini_loop ;if size too small
 
     cmp QWORD [r10 + 1], rcx 
-    jle wfa_fini_loop
+    jl wfa_fini_loop ;if sufficient space but not worst fit
 
     mov rax, r10    ;update max address
     mov rcx, QWORD [r10 + 1]  ;update max size
@@ -92,7 +94,7 @@ wfa_exit:
     ret
 
 
-;memory that properly allocates memory
+;function that properly allocates memory
 memory_alloc:
     push rbp
     mov rbp, rsp
@@ -108,12 +110,57 @@ alloc_middle:
     sub r8, rdi
 
     cmp r8, 10
+    jl am_exit ; if less then 10 extra bytes are available, just returns whole block
+
+    mov QWORD [rax + 1], rdi ;saves size of block
+
+    mov r9, 9
+    add r9, rdi; size of current block
+
+    sub r8, 9 ;available extra space - 9
+
+    mov BYTE [rax + r9], 0
+    mov QWORD [rax + r9 + 1], r8
+
+am_exit:
+    mov BYTE [rax], 1
+    jmp alloc_exit
+    
+alloc_top:
+
+    mov r8, QWORD [cur_brk]  ;saving current brk (register starting point)
+    mov r10, rdi    ;save data size
+
+    mov r9, r8  ;saving into r9 for new brk
+    add r9, 9  ;extends to hold use and size data
+    add r9, r10  ;rdi = current brk +9 + size
+    mov rdi, r9
+
+    mov rax, 12
+    syscall
+
+    ;; add cmp for error here later
+
+    mov QWORD [cur_brk], rax
+    mov BYTE [r8], 1
+    mov QWORD [r8 + 1],  r10  ;old brk is new register staring point
+    mov rax, r8 ;return old brk
 
     jmp alloc_exit
-alloc_top:
 
 alloc_exit:
     pop rbp 
+    ret
+
+
+
+memory_free:
+    push rbp
+    mov rbp, rsp
+
+    mov BYTE [rdi], 0
+
+    pop rbp
     ret
 
 
